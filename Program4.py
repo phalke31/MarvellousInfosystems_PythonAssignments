11cf7aba943188a865f8835733b6b6600c6b1d3c
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-18 12:54:01 +0530

    Add files via upload

---
+++ Program4.py
@@ -0,0 +1,11 @@
+# 4. Write a program which accepts one number and prints the cube of that number.
+
+Number = int(input("Enetr number : "))
+
+def Cube():
+    Ans = int
+    Ans = Number * Number * Number
+    return Ans
+
+Result = Cube()
+print("Cube of number is : ", Result)

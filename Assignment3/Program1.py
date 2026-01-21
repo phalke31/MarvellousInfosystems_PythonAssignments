7571add33e5de26235d286ea0a92a6b200ed8d32
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-21 07:39:06 +0530

    Add files via upload

---
+++ Assignment3/Program1.py
@@ -0,0 +1,15 @@
+""" 1. Write a program which accepts one number and checks whether it is prime or not.
+Input: 11
+Output: Prime Number """
+
+Number = int(input("Enter Number : "))
+
+if Number <= 1:
+    print("Its not a prime number")
+else :
+    for i in range(2, Number):
+        if(Number % i == 0):
+            print("Its not a prime number")
+            break
+    else:
+        print("It's prime number")

d9dda219ffa8adf2361162824af86413d789189d
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-21 07:37:06 +0530

    Delete Assignment3/Program2.py

--- Assignment3/Program2.py
+++
@@ -1,8 +0,0 @@
-""" Write a program which accepts one number and prints count of digits in that number.
-Input: 7521
-Output: 4 """
-
-Number = input("Enetr Number : ")
-Count = len(Number)
-
-print("Length of number : ", Count)

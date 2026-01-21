61cf67f1444c708e9ab101307a60b2e132c9e889
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-21 07:36:46 +0530

    Delete Assignment3/Program4.py

--- Assignment3/Program4.py
+++
@@ -1,8 +0,0 @@
-""" Write a program which accepts one number and prints reverse of that number.
-Input: 123
-Output: 321 """
-
-Number = input("Enter Number : ")
-
-Reverse_number = Number[::-1]
-print("Reverse number is : ", Reverse_number)

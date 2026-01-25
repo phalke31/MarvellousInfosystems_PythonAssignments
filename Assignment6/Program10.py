e4d3b9458e3e2cd924735a7d0f6d421d182a8dc6
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:15:10 +0530

    Delete Assignment6/Program10.py

--- Assignment6/Program10.py
+++
@@ -1,8 +0,0 @@
-"""10. Write a lambda function which accepts three numbers and returns largest number."""
-
-a = int(input("Enter first number : "))
-b = int(input("Enter second number : "))
-c = int(input("Enter third number : "))
-
-Largest_number = lambda a, b, c : a if (a>b and a>c) else b if (b>a and b>c) else c
-print(Largest_number(a,b,c))

b41ff2f7ec38a11edef65b2f68e6d3b25c44bd3a
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:14:19 +0530

    Delete Assignment4/Program3.py

--- Assignment4/Program3.py
+++
@@ -1,9 +0,0 @@
-"""3. Write a program which accepts two numbers and prints addition, subtraction, multiplication and division."""
-
-No1 = int(input("Enter first number : "))
-No2 = int(input("Enter second number : "))
-
-print("Addition : ", No1 + No2)
-print("Subtraction : ", No1 - No2)
-print("Multiplication : ", No1 * No2)
-print("Division : ", No1 / No2)

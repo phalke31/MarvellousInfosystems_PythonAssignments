8d0690fb3d958be6adebea194e1a5e2db0f2036b
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:15:51 +0530

    Delete Assignment6/Program5.py

--- Assignment6/Program5.py
+++
@@ -1,6 +0,0 @@
-"""5. Write a lambda function which accepts one number and returns True if number is even otherwise False."""
-
-a = int(input("Enter Number : "))
-
-Result = lambda a : True if (a % 2==0) else False
-print(Result(a))

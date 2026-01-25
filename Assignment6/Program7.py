dc116eef7476fe401abb662ff89292d620068831
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:16:09 +0530

    Delete Assignment6/Program7.py

--- Assignment6/Program7.py
+++
@@ -1,6 +0,0 @@
-"""7. Write a lambda function which accepts one number and returns True if divisible by 5"""
-
-a = int(input("Enter Number : "))
-
-Result = lambda a : True if (a % 5 == 0) else False
-print(Result(a))

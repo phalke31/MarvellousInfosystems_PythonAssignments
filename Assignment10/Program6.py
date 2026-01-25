2c35afed0c4f357b228b26756cfc5b309c9e2ace
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 13:10:07 +0530

    Delete Assignment10/Program6.py

--- Assignment10/Program6.py
+++
@@ -1,8 +0,0 @@
-"""6. Write a lambda function using reduce() which accepts a list of numbers and returns the minimum element."""
-
-from functools import reduce
-
-Number = [1,2,3,4,5]
-
-Minimum_Number = reduce(lambda x,y : x if x<y else y, Number)
-print("Minimum number is : ", Minimum_Number)

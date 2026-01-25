406428f4433cffb7a8f4cadee756ffd68603e2ca
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:14:59 +0530

    Delete Assignment6/Program1.py

--- Assignment6/Program1.py
+++
@@ -1,9 +0,0 @@
-"""1. Write a lambda function which accepts one number and returns square of that number."""
-
-square = lambda a : a*a
-print(square(2))
-
-a = int(input("Enetr Number : "))
-
-square = lambda a : a*a
-print(square(a))

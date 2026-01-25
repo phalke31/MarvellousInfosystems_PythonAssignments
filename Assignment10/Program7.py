fc0d9663f3433a425f5f9da0c6a32b2453cadf48
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 13:18:34 +0530

    Program7.py

--- Assignment10/Program7.py
+++
@@ -1,7 +0,0 @@
-""" Write a lambda function using filter() which accepts a list of strings and returns a list of strings having length greater than 5."""
-
-String_list = ["Shreya", "Ketaki", "Purva", "Payal", "Anjali"]
-print(String_list)
-
-Length_of_string = list(filter(lambda a : a>5, String_list))
-print(Length_of_string)

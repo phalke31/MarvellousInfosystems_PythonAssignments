c6d6e4088b1f1413ee7f791686147dbf4568015b
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:16:00 +0530

    Delete Assignment6/Program6.py

--- Assignment6/Program6.py
+++
@@ -1,6 +0,0 @@
-"""6. Write a lambda function which accepts one number and returns True if number is odd otherwise False."""
-
-a = int(input("Enter number : "))
-
-Result = lambda a : True if (a % 2 != 0) else False
-print(Result(a))

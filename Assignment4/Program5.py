627eaaae887a340bf545804f4e41db7fa1fbedfc
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:14:39 +0530

    Delete Assignment4/Program5.py

--- Assignment4/Program5.py
+++
@@ -1,8 +0,0 @@
-"""5. Write a program which accepts one number and prints that many numbers in reverse order.
-Input: 5
-Output: 5 4 3 2 1 """
-
-Number = int(input("Enter number : "))
-
-for i in range(Number, 0, -1):
-    print(i)

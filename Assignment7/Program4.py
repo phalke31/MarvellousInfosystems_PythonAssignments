4db92a9c6353d38d4ec92e69378bd67f018510f9
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:20:29 +0530

    Delete Assignment7/Program4.py

--- Assignment7/Program4.py
+++
@@ -1,8 +0,0 @@
-"""4. Write a lambda function using reduce() which accepts a list of numbers and returns the addition of all elements."""
-
-from functools import reduce
-
-Number = [1,2,3,4,5]
-
-Sum =  reduce(lambda x, y : x + y , Number)
-print(Sum)

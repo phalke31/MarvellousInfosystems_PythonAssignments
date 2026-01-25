595c505ee246c36791aef21c9109d65542e3f368
phalke31 <108562834+phalke31@users.noreply.github.com>
2026-01-25 12:34:56 +0530

    Add files via upload

---
+++ Assignment10/Program5.py
@@ -0,0 +1,6 @@
+""" 5. Write a program which accepts one number and prints all odd numbers till that number."""
+
+Number = int(input("Enetr Number : "))
+
+for i in range(1, Number+1, 2):
+    print(i)
